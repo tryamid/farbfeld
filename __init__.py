b36b2eb4345aabcd0a20ee6ada24fad6751efb3d
Tryamid <bsantanu381@gmail.com>
2020-01-31 21:19:18 +0530

    removal of the quantizer code

--- __init__.py
+++ __init__.py
@@ -24,18 +24,9 @@ class FarbfeldEncoder:
     Encoder to encode raw pixels of an arbitary bitdepth ranging
     `1` bit to `64` bit into Farbfeld image of `16` bit pixels.
     """
-    def __init__(self, bitdepth= 8):
-        """
-        bitdepth
-            amount of bits required to represent the input pixels
-            in binary (base 2). Pixels not obeying this limit would
-            result in quantization in a range.
-        """
-        if bitdepth < 1 or bitdepth > 64:
-            raise FarbfeldEncodeError("bit-depth is beyond allowance")
-
-        self._MAX_BIT_VAL = (2 ** bitdepth) - 1
-        self._16B_MAX = 65535
+    def __init__(self, width, height):
+        self.width = abs(width)
+        self.height = abs(height)
 
     def encode(self, outfile, imageframe):
         """
@@ -48,39 +39,14 @@ class FarbfeldEncoder:
             at the top-level, inside an element must contain
             4 elements (sub-pixels). Pixel must be ordered as 'RGBA'.
         """
-        (width, height) = self._wh(imageframe)
-
         if not hasattr(outfile, 'write'):
             raise FarbfeldEncodeError("file-like object doesn't support write() calls")
 
-        outfile.write(b'farbfeld' + pack('<II', width, height))
+        outfile.write(b'farbfeld' + pack('<II', self.width, self.height))
         outfile.write(
-            array('H',
-            map(lambda pix: 
-                int(self._maprange(pix, 
-                    0, self._MAX_BIT_VAL,
-                    0, self._16B_MAX
-            )), chain(*imageframe))
-        ).tobytes())
-
+            array('H', chain(*imageframe))
+        .tobytes())
         outfile.flush()
-    
-    @classmethod
-    def _wh(cls, imgframe):
-        try:
-            height = len(imgframe)
-            width = int(len(imgframe[0]) / 4)
-        except IndexError:
-            raise FarbfeldEncodeError("cannot find a valid 2D plane")
-
-        if not width:
-            raise FarbfeldEncodeError("cannot find a whole pixel of four-components")
-
-        return (width, height)
-
-    @classmethod
-    def _maprange(self, x,a,b,c,d):
-	    return c + ((x - a) * (d - b) / (b - a))
 
 class FarbfeldDecoder:
     """
