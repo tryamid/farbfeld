ce8e916908c48073d80fa046b78a1bb91d0f5dff
Tryamid <bsantanu381@gmail.com>
2020-02-01 14:52:13 +0530

    Traditional configuration to put onto PyPI.

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+import subprocess
+
+setuptools.setup(
+    name= 'farbfeld',
+    version= subprocess.check_output(['git', 'describe', '--tags']).strip() \
+            .decode('utf-8') \
+            .split('-g')[0],
+    description= "Implementation of the Suckelss Farbfeld image format in pure Python",
+    author= "Shantanu Biswas",
+    author_email= "bsantanu381@gmail.com",
+    packages= [
+        'farbfeld'
+    ],
+    url= "https://github.com/tryamid/farbfeld",
+    license= 'WTFPL',
+    python_requires= ">=3.5"
+)
